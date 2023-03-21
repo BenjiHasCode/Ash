67a81bd90dbb3542e4f16af94361a702df2313c9
BenjiHasCode <57215161+BenjiHasCode@users.noreply.github.com>
2023-03-21 18:36:55 +0100

    added precompiled header

---
+++ Ash/src/pch.h
@@ -0,0 +1,21 @@
+#pragma once
+
+// Utility
+#include <iostream>
+#include <memory>
+#include <utility>
+#include <algorithm>
+#include <functional>
+
+// Data structures
+#include <string>
+#include <sstream>
+#include <vector>
+#include <array>
+#include <unordered_map>
+#include <unordered_set>
+
+// Platform specific
+#ifdef DUSTY_PLATFORM_WINDOWS
+#include <Windows.h>
+#endif // DUSTY_PLATFORM_WINDOWS
